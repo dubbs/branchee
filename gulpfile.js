f9b56c94506e53bb32268f97761922709fe34566
Kevin Warrington <kevin.warrington@gmail.com>
2015-06-09 10:54:47 -0600

    update dark rainbow theme

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-gulp.task('compress', function() {
+gulp.task('jsmin', function() {
   return gulp.src('src/*.js')
     .pipe(uglify())
     .pipe(rename({
