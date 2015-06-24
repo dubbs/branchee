87d87a393d0e118bb999953d26f74cb8aae9a956
Kevin Warrington <kevin.warrington@gmail.com>
2015-06-23 21:50:04 -0600

    convert from prototype syntax to function

--- src/js/branchee.js
+++ src/js/branchee.js
@@ -1,226 +1,259 @@
-/**
- * branchee.js
- * version: 1.1.6
- * author: dubbs
- * license: MIT
- */
+
 (function (window, document, $) {
 
-  function Branchee(el, options) {
+  function Branchee($el, options) {
 
-    this.options = options;
+    var $menu = $el.find('.'+options.classMenu);
+    var $menuPanes = null;
 
-    this.$el = $(el);
-    this.$menu = this.$el.find('.'+this.options.classMenu);
+    /**
+     * Clear CSS Transitions
+     */
+    var clearTransitions = function () {
+      var classList = [
+        options.classTransitionExitRight,
+        options.classTransitionExitLeft,
+        options.classTransitionEnterRight,
+        options.classTransitionEnterLeft
+      ].join(' ');
+      $menuPanes.removeClass(classList);
+    };
 
-    this.$panes = null;
+    /**
+     *
+     */
+    var setActivePane = function ($paneTarget) {
+      $menuPanes.removeClass(options.classMenuPaneActive);
+      $paneTarget.addClass(options.classMenuPaneActive);
+    };
 
-    this.$el.on('click', '.'+this.options.classMenuPaneTarget, $.proxy(this._handleClick, this));
+    /**
+     *
+     */
+    var toggleOpen = function () {
+      var id = $el.attr('id');
 
-    if (this.options.handleToggle) {
-      $(document).on('click', '.'+this.options.classToggleMenu, $.proxy(this._handleToggle, this));
+      // toggle anchor
+      var $toggle = $('.branchee-toggle-menu[href="#'+id+'"]');
+      $toggle.toggleClass(options.classOpen);
+
+      // toggle menu
+      $el.toggleClass(options.classOpen);
+
+      clearTransitions();
+
+      options.onAfterToggle.call(this, $toggle);
+    };
+
+    /**
+     *
+     */
+    var handleToggle = function (e) {
+      e.preventDefault();
+      if ($el.attr('id') === $(e.currentTarget).attr('href').substring(1)) {
+        toggleOpen();
+      }
+    };
+
+    /**
+     *
+     */
+    var handleClick = function (e) {
+
+      e.preventDefault();
+      var $target = $(e.target);
+      // go up to menu pane
+      var $paneCurrent = $target.closest('.'+options.classMenuPane);
+
+      var $paneTarget = $el.find('['+options.dataPaneId+'="' + $target.data(options.classMenuPaneTarget) + '"]');
+
+      clearTransitions();
+
+      if($target.closest('.'+options.classBackLink).length !== 0) {
+        // back
+        $paneCurrent.addClass(options.classTransitionExitRight);
+        $paneTarget.addClass(options.classTransitionEnterLeft);
+      } else {
+        // forward
+        $paneCurrent.addClass(options.classTransitionExitLeft);
+        $paneTarget.addClass(options.classTransitionEnterRight);
+      }
+
+      setActivePane($paneTarget);
+
+    };
+
+    /**
+     * Generate a unique pane id
+     */
+    var generateId = function () {
+      return Math.random().toString(36).slice(2);
+    };
+
+    /**
+     *
+     */
+    var addRootId = function () {
+      $menu.find('> ul').attr(options.dataPaneId, generateId());
+    };
+
+    /**
+     *
+     */
+    var addSectionLabel = function (index, element) {
+      $(element).addClass(options.classSection + ' ' + options.classSectionPrefix+(index+1));
+      $(element).find('ul').addClass(options.classSection + ' ' + options.classSectionPrefix+(index+1));
+    };
+
+    /**
+     *
+     */
+    var addSectionLabels = function () {
+      $menu.children('ul').children('li').each(addSectionLabel);
+    };
+
+    /**
+     *
+     */
+    var appendPaneTargets = function (index, element) {
+      var paneTargetId = generateId();
+      $(element).siblings('ul').attr(options.dataPaneId, paneTargetId);
+      $(element).append('<span class="'+options.classMenuPaneTarget+'" '+options.dataPaneTarget+'="'+paneTargetId+'"></span>');
+    };
+
+    /**
+     *
+     */
+    var addPaneTargets = function () {
+      // a + ul ?
+      var $itemsWithPaneTarget = $menu.find('ul').parents('li').children('a');
+      $itemsWithPaneTarget.each(appendPaneTargets);
+    };
+
+    /**
+     *
+     */
+    var addMenuToggleTarget = function () {
+      var id = $el.attr('id');
+      var $toggle = $('.branchee-toggle-menu[href="#'+id+'"]');
+      $toggle.prepend('<span class="branchee-toggle-menu-icon"></span>');
+    };
+
+    /**
+     *
+     */
+    var addBackLink = function (index, element) {
+      var $a = $(element).siblings('a').clone();
+      // update pane target
+      var paneId = $(element).parents('ul').attr(options.dataPaneId);
+      $a.children('span').attr(options.dataPaneTarget, paneId);
+      // add as first child to pane
+      $a.prependTo($(element)).wrap("<li class='"+options.classBackLink+"'></li>");
+    };
+
+    /**
+     *
+     */
+    var addBackLinks = function () {
+      var $panes = $menu.find('['+options.dataPaneId+']');
+      $panes.each(addBackLink);
+    };
+
+    /**
+     *
+     */
+    var createPane = function (index, element) {
+      // copy ul and remove children
+      var $ul = $(element).clone();
+      $ul.find('ul').remove();
+
+      // create wrapper fragment and copy pane id
+      var $wrapper = $('<div class="'+options.classMenuPane+'"></div>');
+      $wrapper.attr(options.dataPaneId, $ul.attr(options.dataPaneId));
+
+      // remove existing pane id on ul and wrap
+      $ul.removeAttr(options.dataPaneId).appendTo($menu).wrap($wrapper);
+
+    };
+
+    /**
+     *
+     */
+    var createPanes = function () {
+      var $panes = $menu.find('['+options.dataPaneId+']');
+      $panes.each(createPane);
+      $menuPanes = $menu.find('.'+options.classMenuPane);
+    };
+
+    /**
+     *
+     */
+    var removeOriginalMenu = function () {
+      $menu.find('> ul').remove();
+    };
+
+    /**
+     *
+     */
+    var init = function () {
+
+      options.onBeforeInit.call(this);
+
+      addRootId();
+      addSectionLabels();
+      addPaneTargets();
+      addMenuToggleTarget();
+      addBackLinks();
+      createPanes();
+      removeOriginalMenu();
+      setActivePane($menuPanes.first());
+
+      options.onAfterInit.call(this);
+
+    };
+
+
+    /**
+     *
+     */
+    $el.on('click', '.'+options.classMenuPaneTarget, handleClick);
+
+    /**
+     *
+     */
+    if (options.handleToggle) {
+      $(document).on('click', '.'+options.classToggleMenu, handleToggle);
     }
+
+    /**
+     * Public API
+     */
+    return {
+      init: init,
+      toggleOpen: toggleOpen,
+      clearTransitions: clearTransitions
+    };
+
   }
 
-  /********
-    PUBLIC
-   *********/
-  Branchee.prototype.init = function() {
-
-    this.options.onBeforeInit.call(this);
-
-    this._addRootId();
-    this._addSectionLabels();
-    this._addPaneTargets();
-    this._addMenuToggleTarget();
-    this._addBackLinks();
-    this._createPanes();
-    this._removeOriginalMenu();
-    this._setActivePane(this.$panes.first());
-
-    this.options.onAfterInit.call(this);
-  };
-
-  Branchee.prototype.toggleOpen = function() {
-
-    var id = this.$el.attr('id');
-
-    // toggle anchor
-    var $toggle = $('.branchee-toggle-menu[href="#'+id+'"]');
-    $toggle.toggleClass(this.options.classOpen);
-
-    // toggle menu
-    this.$el.toggleClass(this.options.classOpen);
-
-    this._clearTransitions();
-
-    this.options.onAfterToggle.call(this, $toggle);
-  };
-
-  Branchee.prototype.getActivePane = function() {
-    return this.$panes.filter('.'+this.options.classMenuPaneActive);
-  };
-
-  Branchee.prototype.getActivePaneByPath = function(path) {
-    var $activeLinks = this.$menu.find('a[href="'+path+'"]');
-    $activeLinks.addClass(this.options.classMenuItemActive);
-
-    var $activeLinksLI = $activeLinks.closest('li');
-
-    var $activeLinksWithBack = $activeLinksLI.filter('.'+this.options.classBackLink);
-    if ($activeLinksWithBack.length) {
-      return $activeLinksWithBack.closest('.'+this.options.classMenuPane);
-    }
-
-    if ($activeLinksLI.first().length) {
-      return $activeLinksLI.first().closest('.'+this.options.classMenuPane);
-    }
-  };
-
-  Branchee.prototype.setActivePane = function($paneTarget) {
-    this._setActivePane($paneTarget);
-  };
-
-  /********
-    PRIVATE
-   *********/
-  Branchee.prototype._addRootId = function () {
-    this.$menu.find('> ul').attr(this.options.dataPaneId, this._generateId());
-  };
-
-  Branchee.prototype._addSectionLabels = function () {
-    this.$menu.children('ul').children('li').each($.proxy(this._addSectionLabel, this));
-  };
-
-  Branchee.prototype._addSectionLabel = function (index, element) {
-    $(element).addClass(this.options.classSection + ' ' + this.options.classSectionPrefix+(index+1));
-    $(element).find('ul').addClass(this.options.classSection + ' ' + this.options.classSectionPrefix+(index+1));
-  };
-
-  Branchee.prototype._addMenuToggleTarget = function () {
-    var id = this.$el.attr('id');
-    var $toggle = $('.branchee-toggle-menu[href="#'+id+'"]');
-    $toggle.prepend('<span class="branchee-toggle-menu-icon"></span>');
-  };
-
-  Branchee.prototype._addPaneTargets = function () {
-    // a + ul ?
-    var $itemsWithPaneTarget = this.$menu.find('ul').parents('li').children('a');
-    $itemsWithPaneTarget.each($.proxy(this._appendPaneTargets, this));
-  };
-
-  Branchee.prototype._appendPaneTargets = function (index, element) {
-    var paneTargetId = this._generateId();
-    $(element).siblings('ul').attr(this.options.dataPaneId, paneTargetId);
-    $(element).append('<span class="'+this.options.classMenuPaneTarget+'" '+this.options.dataPaneTarget+'="'+paneTargetId+'"></span>');
-  };
-
-  Branchee.prototype._addBackLinks = function () {
-    var $panes = this.$menu.find('['+this.options.dataPaneId+']');
-    $panes.each($.proxy(this._addBackLink, this));
-  };
-
-  Branchee.prototype._addBackLink = function (index, element) {
-    var $a = $(element).siblings('a').clone();
-    // update pane target
-    var paneId = $(element).parents('ul').attr(this.options.dataPaneId);
-    $a.children('span').attr(this.options.dataPaneTarget, paneId);
-    // add as first child to pane
-    $a.prependTo($(element)).wrap("<li class='"+this.options.classBackLink+"'></li>");
-  };
-
-  Branchee.prototype._createPanes = function () {
-    var $panes = this.$menu.find('['+this.options.dataPaneId+']');
-    $panes.each($.proxy(this._createPane, this));
-    this.$panes = this.$menu.find('.'+this.options.classMenuPane);
-  };
-
-  Branchee.prototype._createPane = function (index, element) {
-    // copy ul and remove children
-    var $ul = $(element).clone();
-    $ul.find('ul').remove();
-
-    // create wrapper fragment and copy pane id
-    var $wrapper = $('<div class="'+this.options.classMenuPane+'"></div>');
-    $wrapper.attr(this.options.dataPaneId, $ul.attr(this.options.dataPaneId));
-
-    // remove existing pane id on ul and wrap
-    $ul.removeAttr(this.options.dataPaneId).appendTo(this.$menu).wrap($wrapper);
-  };
-
-  Branchee.prototype._removeOriginalMenu = function () {
-    this.$menu.find('> ul').remove();
-  };
-
-  Branchee.prototype._generateId = function() {
-    return Math.random().toString(36).slice(2);
-  };
-
-  Branchee.prototype._handleClick = function(e) {
-    e.preventDefault();
-    var $target = $(e.target);
-    // go up to menu pane
-    var $paneCurrent = $target.closest('.'+this.options.classMenuPane);
-
-    var $paneTarget = this.$el.find('['+this.options.dataPaneId+'="' + $target.data(this.options.classMenuPaneTarget) + '"]');
-
-    this._clearTransitions();
-
-    if($target.closest('.'+this.options.classBackLink).length !== 0) {
-      // back
-      $paneCurrent.addClass(this.options.classTransitionExitRight);
-      $paneTarget.addClass(this.options.classTransitionEnterLeft);
-    } else {
-      // forward
-      $paneCurrent.addClass(this.options.classTransitionExitLeft);
-      $paneTarget.addClass(this.options.classTransitionEnterRight);
-    }
-
-    this._setActivePane($paneTarget);
-  };
-
-  Branchee.prototype._handleToggle = function(e) {
-    e.preventDefault();
-
-    if (this.$el.attr('id') === $(e.currentTarget).attr('href').substring(1)) {
-      this.toggleOpen();
-    }
-  };
-
-  Branchee.prototype._clearTransitions = function() {
-    var classList = [
-      this.options.classTransitionExitRight,
-      this.options.classTransitionExitLeft,
-      this.options.classTransitionEnterRight,
-      this.options.classTransitionEnterLeft
-    ].join(' ');
-    this.$panes.removeClass(classList);
-  };
-
-  Branchee.prototype._setActivePane = function($paneTarget) {
-    this.$panes.removeClass(this.options.classMenuPaneActive);
-    $paneTarget.addClass(this.options.classMenuPaneActive);
-  };
-
-
-  this.Branchee = Branchee;
-
-  // JQUERY
+  /**
+   * jQuery function
+   */
   $.fn.branchee = function(options) {
 
+    options = $.extend({}, $.fn.branchee.defaults, options);
+
     return this.each(function() {
-
-      var config = $.extend({}, $.fn.branchee.defaults, options);
-      var branchee = new Branchee(this, config);
-      branchee.init();
-
-      // store instance on dom element
-      $(this).data('branchee', branchee);
-
+      var $el = $(this);
+      var branchee = Branchee($el, options).init();
+      $el.data('branchee', branchee);
     });
 
   };
 
+  /**
+   * jQuery defauls
+   */
   $.fn.branchee.defaults = {
     handleToggle:              true,
     classBackLink:             'branchee-back',
